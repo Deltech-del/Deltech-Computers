9ba4fc5f682c0ed1b96d21168dc702cdd852907a
Gerald Okoth <okothgerald449@gmail.com>
2024-11-25 23:55:31 +0300

    Create components

--- deltech/src/App.js
+++ deltech/src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import NavigationBar from './components/Navigation/NavigationBar';
+import AboutUs from './components/About/AboutUs';
+import MainPage from './components/MainPage/MainPage';
+import Services from './components/Services/Services';
+import Faq from './components/Faq/Faq';
+import ContactUs from './components/Contact/ContactUs';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavigationBar />
+      <MainPage />
+      <AboutUs />
+      <Services />
+      <Faq />
+      <ContactUs />
     </div>
   );
 }
