b1fcf91b20b8a5ba73301beac7e8605293caedb6
Gerald Okoth <okothgerald449@gmail.com>
2024-11-28 10:28:38 +0300

    Create components

--- deltech/src/App.js
+++ deltech/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import NavigationBar from './components/Navigation/NavigationBar';
-import AboutUs from './components/About/AboutUs';
-import MainPage from './components/MainPage/MainPage';
-import Services from './components/Services/Services';
-import Faq from './components/Faq/Faq';
-import ContactUs from './components/Contact/ContactUs';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutUs from '.pages/About/AboutUs';
+import MainPage from '.pages/MainPage/MainPage';
+import Services from '.pages/Services/Services';
+import Faq from '.pages/Faq/Faq';
+import ContactUs from '.pages/Contact/ContactUs';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      <MainPage />
-      <AboutUs />
-      <Services />
-      <Faq />
-      <ContactUs />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}/>
+          <Route path="./pages/About" element={<AboutUs />}/>
+          <Route path="./pages/Services" element={<Services />}/>
+          <Route path="./pages/Faq" element={<Faq />}/>
+          <Route path="./pages/Contact" element={<ContactUs />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
